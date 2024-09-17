134bb2ed6d723f89b77767a79e51e57f87205180
akiooo45 <huoji302562020@gmail.com>
2024-09-17 14:52:10 +0800

    by aiohttp

--- booksource.py
+++ booksource.py
@@ -1,46 +1,67 @@
-import requests
-import json
+import aiohttp
+import asyncio
+import orjson
 import urllib.parse
 
+# Load the data from the JSON file
+with open("./info.json") as f:
+    data = orjson.loads(f.read())
 
-username = "admin"
-password = "admin"
-remote_booksource = "https://jihulab.com/aoaostar/legado/-/raw/release/cache/1b8256c78b385543b5e8aa6a0d7693c76f8e60d4.json"
-reader_addr = "http://127.0.0.1:4396"
-session = requests.session()
+username = data["username"]
+password = data["password"]
+remote_bs = data["remote_booksource"]
+reader_addr = data["reader_addr"]
 
 
-def login():
+# 登录到reader3
+async def login(session):
     login_url = "/reader3/login"
     url = urllib.parse.urljoin(reader_addr, login_url)
-    # print(url)
     data = {"username": username, "password": password, "isLogin": True}
-    headers = {
-        "Content-Type": "application/json",
-    }
-    session.post(url, data=json.dumps(data), headers=headers).json()
+    headers = {"Content-Type": "application/json"}
+
+    async with session.post(url, json=data, headers=headers) as response:
+        result = await response.json()
+        print(result)
+        print("\n")
 
 
-def deleteBookSource():
+# 删除reader3本地书源
+async def deleteBookSource(session):
     deleteBookSource_url = "/reader3/deleteAllBookSources"
     url = urllib.parse.urljoin(reader_addr, deleteBookSource_url)
-    print(session.post(url).json())
+
+    async with session.post(url) as response:
+        result = await response.json()
+        print(result)
 
 
-def get_remotebs():
-    return requests.get(remote_booksource).json()
+# 获取远程书源
+async def get_remotebs(session):
+    for i in remote_bs:
+        async with session.get(i) as response:
+            return await response.json()
 
 
-def send_bs():
+# 推送书源到reader3
+async def send_bs(session):
     remote_booksource = "/reader3/saveBookSources"
     url = urllib.parse.urljoin(reader_addr, remote_booksource)
-    headers = {
-        "Content-Type": "application/json",
-    }
-    data = get_remotebs()
-    print(session.post(url, json=data, headers=headers).json())
+    headers = {"Content-Type": "application/json"}
+
+    payload = await get_remotebs(session)
+    async with session.post(url, json=payload, headers=headers) as response:
+        result = await response.json()
+        print(result)
 
 
-login()
-deleteBookSource()
-send_bs()
+# Main function to run all tasks
+async def main():
+    async with aiohttp.ClientSession() as session:
+        await login(session)
+        await deleteBookSource(session)
+        await send_bs(session)
+
+
+# Run the main function
+asyncio.run(main())
